bc9edabb01ebd09d35fd3e78523f5130b0f66bf6
Diego Pais <diego.pais29@gmail.com>
2016-06-24 16:43:45 -0300

    Changes to support a custom FlowLayout

---
+++ Pod/Classes/PaperCollectionViewFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  PaperCollectionViewFlowLayout.h
+//  Pods
+//
+//  Created by Diego Pais on 6/21/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaperCollectionViewFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ Pod/Classes/PaperCollectionViewFlowLayout.m
@@ -0,0 +1,50 @@
+//
+//  PaperCollectionViewFlowLayout.m
+//  Pods
+//
+//  Created by Diego Pais on 6/21/16.
+//
+//
+
+#import "PaperCollectionViewFlowLayout.h"
+
+@implementation PaperCollectionViewFlowLayout
+
+- (CGPoint)contentOffsetAfterItems:(NSInteger)itemsCount
+{
+    CGPoint contentOffset   = CGPointZero;
+    CGFloat spacing         = itemsCount * self.minimumInteritemSpacing;
+    CGFloat itemsWidth      = itemsCount * self.itemSize.width;
+    contentOffset.x         = itemsWidth + spacing;
+    contentOffset.x         = MAX(self.collectionView.bounds.size.width, contentOffset.x);
+    
+    return contentOffset;
+}
+
+- (UICollectionViewLayoutAttributes *)initialLayoutAttributesForAppearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
+{
+    UICollectionViewLayoutAttributes *attributes = [super initialLayoutAttributesForAppearingItemAtIndexPath:itemIndexPath];
+    
+    CGFloat offset = [self contentOffsetAfterItems:3].x;
+    
+    attributes.transform = CGAffineTransformMakeTranslation(offset, 0.f);
+    attributes.zIndex   = 50;
+    attributes.alpha    = 1.0f;
+
+    return attributes;
+}
+
+- (UICollectionViewLayoutAttributes *)finalLayoutAttributesForDisappearingItemAtIndexPath:(NSIndexPath *)itemIndexPath
+{
+    UICollectionViewLayoutAttributes *attributes = [super finalLayoutAttributesForDisappearingItemAtIndexPath:itemIndexPath];
+    
+    CGFloat offset = [self contentOffsetAfterItems:3].x;
+    
+    attributes.transform = CGAffineTransformMakeTranslation(-offset, 0.f);
+    attributes.zIndex = 100;
+    attributes.alpha = 1.0f;
+    
+    return attributes;
+}
+
+@end
